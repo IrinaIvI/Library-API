cf754f7bd0d55edae22e81aa47ce50179a6281cc
Irina Ivashko <ira.ivashko.99@gmail.com>
2024-12-10 20:05:00 +0300

    Создание асинхронной сессии к бд, инициализация роутеров

---
+++ src/app/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
+from typing import AsyncGenerator
+from app.config import DATABASE_URL
+
+engine = create_async_engine(url=DATABASE_URL, echo=True)
+async_session = async_sessionmaker(bind=engine, class_=AsyncSession)
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
