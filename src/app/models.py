b1ef0281c60779cc017ccfb519a1e08b7a20c13f
Irina Ivashko <ira.ivashko.99@gmail.com>
2024-12-10 20:37:42 +0300

    Создание моделей

---
+++ src/app/models.py
@@ -0,0 +1,39 @@
+from sqlalchemy import String, Date, Text, ForeignKey,Integer
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+class Base(DeclarativeBase):
+    pass
+
+class Author(Base):
+    __tablename__ = "author"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(10), nullable=False)
+    surname: Mapped[str] = mapped_column(String(10), nullable=False)
+    date_of_birth: Mapped[Date] = mapped_column(nullable=True)
+
+    books: Mapped[list["Book"]] = relationship("Book", back_populates="author")
+
+class Book(Base):
+    __tablename__ = "book"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(20), nullable=False)
+    description: Mapped[str] = mapped_column(Text(200), nullable=True)
+    author_id: Mapped[int] = mapped_column(ForeignKey("author.id"), nullable=False)
+    available_copies: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+
+    author: Mapped["Author"] = relationship(back_populates="books")
+    borrows: Mapped[list["Borrow"]] = relationship("Borrow", back_populates="book")
+
+class Borrow(Base):
+    __tablename__ = "borrow"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    book_id: Mapped[int] = mapped_column(ForeignKey("book.id"), nullable=False)
+    reader_name: Mapped[str] = mapped_column(String(10), nullable=False)
+    borrow_date: Mapped[Date] = mapped_column(nullable=False)
+    return_date: Mapped[Date] = mapped_column(nullable=False)
+
+    book: Mapped["Book"] = relationship("Book", back_populates="borrows")
+
