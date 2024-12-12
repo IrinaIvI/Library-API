ba32a29ed742937d8c1e03a13f520e630958a090
Irina Ivashko <ira.ivashko.99@gmail.com>
2024-12-12 21:47:36 +0300

    Созданы crud операции для Book и Author

---
+++ src/app/crud/author.py
@@ -0,0 +1,86 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import asc, func, text
+from ..models import Author
+from datetime import date
+from ..schemes import AuthorScheme
+from sqlalchemy.future import select
+
+
+async def create_author(name: str, surname: str, date_of_birth: date, db: AsyncSession) -> AuthorScheme:
+    try:
+
+        result = await db.execute(select(func.count()).select_from(Author))
+        count = result.scalar()
+
+        if count == 0:
+            await db.execute(text("SELECT setval(pg_get_serial_sequence('author', 'id'), 1, false);"))
+            await db.commit()
+
+        new_author = Author(name=name, surname=surname, date_of_birth=date_of_birth)
+        db.add(new_author)
+        await db.commit()
+        await db.refresh(new_author)
+        return AuthorScheme(
+            id=new_author.id, 
+            name=new_author.name, 
+            surname=new_author.surname, 
+            date_of_birth=new_author.date_of_birth
+        )
+    except Exception:
+        await db.rollback()
+        raise
+    
+async def get_all_authors(db: AsyncSession) -> list[AuthorScheme]:
+    try: 
+        result = await db.execute(select(Author).order_by(asc(Author.id)))
+        authors = result.scalars().all()
+        return authors
+    except Exception:
+        raise
+
+async def get_author(id: int, db: AsyncSession) -> AuthorScheme:
+    try:
+        result = await db.execute(select(Author).filter(Author.id == id))
+        author = result.scalars().first()
+        return AuthorScheme(
+            id=author.id, 
+            name=author.name, 
+            surname=author.surname, 
+            date_of_birth=author.date_of_birth
+        )
+    except Exception:
+        raise
+    
+async def update_author(id: int, name: str, surname: str, date_of_birth: date, db: AsyncSession) -> AuthorScheme:
+    try:
+        result = await db.execute(select(Author).filter(Author.id == id))
+        current_author = result.scalars().first()
+
+        if not current_author:
+            return None
+
+        current_author.name = name
+        current_author.surname = surname
+        current_author.date_of_birth = date_of_birth
+        await db.commit()
+        await db.refresh(current_author)
+        return AuthorScheme(id=current_author.id, name=current_author.name, surname=current_author.surname, date_of_birth=current_author.date_of_birth)
+    
+    except Exception:
+        await db.rollback()
+        raise
+
+async def delete_author(id: int, db: AsyncSession) -> bool:
+    try:
+        result = await db.execute(select(Author).filter(Author.id == id))
+        current_author = result.scalars().first()
+        if not current_author:
+            return False
+        
+        await db.delete(current_author)
+        await db.commit()
+        return True
+    except Exception:
+        await db.rollback()
+        raise
+
---
+++ src/app/crud/book.py
@@ -0,0 +1,94 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import asc, func, text
+from ..models import Book
+from datetime import date
+from ..schemes import BookScheme
+from sqlalchemy.future import select
+
+
+async def create_book(title: str, description: str, author_id: int, available_copies: int, db: AsyncSession) -> BookScheme:
+    try:
+
+        result = await db.execute(select(func.count()).select_from(Book))
+        count = result.scalar()
+
+        if count == 0:
+            await db.execute(text("SELECT setval(pg_get_serial_sequence('book', 'id'), 1, false);"))
+            await db.commit()
+
+        new_book = Book(title=title, description=description, author_id=author_id, available_copies=available_copies)
+        db.add(new_book)
+        await db.commit()
+        await db.refresh(new_book)
+        return BookScheme(
+            id=new_book.id, 
+            title=new_book.title, 
+            description=new_book.description, 
+            author_id=new_book.author_id,
+            available_copies=new_book.available_copies
+        )
+    except Exception:
+        await db.rollback()
+        raise
+    
+async def get_all_books(db: AsyncSession) -> list[BookScheme]:
+    try: 
+        result = await db.execute(select(Book).order_by(asc(Book.id)))
+        books = result.scalars().all()
+        return books
+    except Exception:
+        raise
+
+async def get_book(id: int, db: AsyncSession) -> BookScheme:
+    try:
+        result = await db.execute(select(Book).filter(Book.id == id))
+        book = result.scalars().first()
+        return BookScheme(
+            id=book.id, 
+            title=book.title, 
+            description=book.description, 
+            author_id=book.author_id,
+            available_copies=book.available_copies
+        )
+    except Exception:
+        raise
+    
+async def update_book(id: int, title: str, description: str, author_id: int, available_copies: int, db: AsyncSession) -> BookScheme:
+    try:
+        result = await db.execute(select(Book).filter(Book.id == id))
+        current_book = result.scalars().first()
+
+        if not current_book:
+            return None
+
+        current_book.title = title
+        current_book.description = description
+        current_book.author_id = author_id
+        await db.commit()
+        await db.refresh(current_book)
+        return BookScheme(
+            id=current_book.id, 
+            title=current_book.title, 
+            description=current_book.description, 
+            author_id=current_book.author_id,
+            available_copies=current_book.available_copies
+        )
+    
+    except Exception:
+        await db.rollback()
+        raise
+
+async def delete_book(id: int, db: AsyncSession) -> bool:
+    try:
+        result = await db.execute(select(Book).filter(Book.id == id))
+        current_book = result.scalars().first()
+        if not current_book:
+            return False
+        
+        await db.delete(current_book)
+        await db.commit()
+        return True
+    except Exception:
+        await db.rollback()
+        raise
+
