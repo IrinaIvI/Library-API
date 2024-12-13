59d9fbdec49593dcf0890175b1f7908ba8a15a42
Irina Ivashko <ira.ivashko.99@gmail.com>
2024-12-13 19:08:15 +0300

    Обновление миграций

--- src/app/models.py
+++ src/app/models.py
@@ -9,8 +9,8 @@ class Author(Base):
     __tablename__ = "author"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(10), nullable=False)
-    surname: Mapped[str] = mapped_column(String(10), nullable=False)
+    name: Mapped[str] = mapped_column(String(20), nullable=False)
+    surname: Mapped[str] = mapped_column(String(20), nullable=False)
     date_of_birth: Mapped[date] = mapped_column(nullable=True)
 
     books: Mapped[list["Book"]] = relationship("Book", back_populates="author")
@@ -21,7 +21,7 @@ class Book(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(20), nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=True)
-    author_id: Mapped[int] = mapped_column(ForeignKey("author.id"), nullable=False)
+    author_id: Mapped[int] = mapped_column(ForeignKey("author.id", ondelete='CASCADE'), nullable=False)
     available_copies: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
 
     author: Mapped["Author"] = relationship(back_populates="books")
@@ -31,8 +31,8 @@ class Borrow(Base):
     __tablename__ = "borrow"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    book_id: Mapped[int] = mapped_column(ForeignKey("book.id"), nullable=False)
-    reader_name: Mapped[str] = mapped_column(String(10), nullable=False)
+    book_id: Mapped[int] = mapped_column(ForeignKey("book.id", ondelete='CASCADE'), nullable=False)
+    reader_name: Mapped[str] = mapped_column(String(20), nullable=False)
     borrow_date: Mapped[date] = mapped_column(nullable=False)
     return_date: Mapped[date] = mapped_column(nullable=True)
 
